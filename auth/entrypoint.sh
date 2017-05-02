31bfaecf9307715305949460668f1771b2bb781a
Aaron Ault <ault@purdue.edu>
2017-05-01 23:52:30 -0400

    Turned on all debugging, updated oada-ref-auth-js to latest version of code.  We now can get tokens!  Next step is to make sure the objects we're saving are the objects that token-lookup service needs

--- auth/entrypoint.sh
+++ auth/entrypoint.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
 cd /code/auth/oada-ref-auth-js && \
-  DEBUG="init,arango/init,err" npm run init -- --config=../config.js && \
-  DEBUG="info,error" npm run start -- --config=../config.js
+  DEBUG="*" npm run init -- --config=../config.js && \
+  DEBUG="*" npm run start -- --config=../config.js
 
