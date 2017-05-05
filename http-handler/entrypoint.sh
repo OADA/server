9d4935137ce63c492349d8092fcd68705f2ce50a
Yaguang Zhang <yaguangz@outlook.com>
2017-05-04 20:39:04 -0400

    Setup test structure for http-handler (by Alex).
      - Succeed in hitting http-handler & monitoring Kafka simutaneously.

--- http-handler/entrypoint.sh
+++ http-handler/entrypoint.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 
 cd /code/http-handler/oada-srvc-http-handler&& \
+  npm install && \
   npm run start
--- tests/entrypoint.sh
+++ tests/entrypoint.sh
@@ -1,1 +1,5 @@
 #! /bin/sh
+
+cd /code/tests/oada-srvc-tests && \
+  npm install && \
+  npm run start
