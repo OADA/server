7b7888407227d16fca26cd01805c2107a174da1a
Andrew Balmos <andrew@balmos.org>
2017-05-06 15:48:28 -0400

    Move to yarn for speed and yarn.lock

---
+++ admin/do-yarn-install.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -i
+
+CDIR=`pwd`
+
+echo "----------"
+echo "well-known"
+echo "----------"
+cd /code/well-known/oada-srvc-well-known
+yarn install
+
+echo
+echo "----"
+echo "auth"
+echo "----"
+cd /code/auth/oada-ref-auth-js
+yarn install
+
+echo
+echo "------------"
+echo "graph-lookup"
+echo "------------"
+cd /code/graph-lookup/oada-srvc-graph-lookup
+yarn install
+
+echo
+echo "------------"
+echo "token-lookup"
+echo "------------"
+cd /code/token-lookup/oada-srvc-token-lookup
+yarn install
+
+echo
+echo "------------"
+echo "http-handler"
+echo "------------"
+cd /code/http-handler/oada-srvc-http-handler
+yarn install
+
+echo
+echo "-----"
+echo "tests"
+echo "-----"
+cd /code/tests/oada-srvc-tests
+yarn install
+
+cd $CDIR
