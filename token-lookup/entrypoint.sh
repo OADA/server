e0067b7cd53bdc752655dce9269dc08771cae580
Andrew Balmos <andrew@balmos.org>
2017-05-06 16:11:52 -0400

    Fix token-lookup entrypoint

--- token-lookup/entrypoint.sh
+++ token-lookup/entrypoint.sh
@@ -2,4 +2,4 @@
 
 # Use the same config file as auth to get proper db/collections names
 cd /code/token-lookup/oada-srvc-token-lookup && \
-  DEBUG="*" npm run start -- --config='../config.js' --authconfig='../../../auth/config.js'
+  DEBUG="*" npm run start
