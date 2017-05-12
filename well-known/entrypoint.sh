2343cce5cb2b986f77db9f82627caad1cda95cc5
Aaron Ault <ault@purdue.edu>
2017-05-12 17:56:33 -0400

    fixed entrypoint problem

--- well-known/entrypoint.sh
+++ well-known/entrypoint.sh
@@ -3,6 +3,5 @@
 # Note that the path to the overriding config file must be relative
 # to index.js inside the service directory because it will be required
 # there.
-echo 'Running entrypoint command: cd /code/well-known/oada-srvc-well-known && DEBUG="http/info,http/trace" npm run start -- --config=../config.js'
 cd /code/well-known/oada-srvc-well-known && \
-  DEBUG="http/info,http/trace" npm run start -- --config=../config.js
+  DEBUG="*" npm run start -- --config=/oada-srvc-docker-config.js
