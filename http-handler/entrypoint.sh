521c95f2e15552be6add6e445fa3b747e846e184
Aaron Ault <ault@purdue.edu>
2017-05-12 14:42:18 -0400

    Added wait-for to http-handler to wait for startup

--- http-handler/entrypoint.sh
+++ http-handler/entrypoint.sh
@@ -1,4 +1,6 @@
 #! /bin/sh
 
-cd /code/http-handler/oada-srvc-http-handler&& \
+chmod u+x /code/http-handler/wait-for-it.sh && \
+  /code/http-handler/wait-for-it.sh startup:80 -t 0 && \
+  cd /code/http-handler/oada-srvc-http-handler && \
   npm run start
