93b8f0b00e887c2af137ddc7ce7ec0723be23ffc
Aaron Ault <ault@purdue.edu>
2017-05-12 13:58:13 -0400

    Added startup, and auth waits for it.  Adding to others as needed now.

--- auth/entrypoint.sh
+++ auth/entrypoint.sh
@@ -1,6 +1,8 @@
 #! /bin/sh
 
-cd /code/auth/oada-ref-auth-js && \
+chmod u+x /code/auth/wait-for-it.sh && \
+  /code/auth/wait-for-it.sh startup:80 -t 0 && \
+  cd /code/auth/oada-ref-auth-js && \
   DEBUG="*" npm run init -- --config=../config.js && \
   DEBUG="*" npm run start -- --config=../config.js
 
--- startup/entrypoint.sh
+++ startup/entrypoint.sh
@@ -1,4 +1,8 @@
 #! /bin/sh
 
-cd /code/startup/oada-srvc-startup && \
+chmod u+x /code/startup/wait-for-it.sh && \
+  /code/startup/wait-for-it.sh arangodb:8529 -t 0 && \
+  /code/startup/wait-for-it.sh zookeeper:2181 -t 0 && \
+  /code/startup/wait-for-it.sh kafka:9092 -t 0 && \
+  cd /code/startup/oada-srvc-startup && \
   npm run start
