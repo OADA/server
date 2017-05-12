6de14c2ef15297ca0b8d556feab441fca6e93e1c
Aaron Ault <ault@purdue.edu>
2017-05-12 14:07:38 -0400

    Added restart: always to all services

---
+++ proxy/entrypoint.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+chmod u+x /code/proxy/wait-for-it.sh && \
+  /code/proxy/wait-for-it.sh arangodb:8529 -t 0 && \
+  /code/proxy/wait-for-it.sh zookeeper:2181 -t 0 && \
+  /code/proxy/wait-for-it.sh kafka:9092 -t 0 && \
+  /code/proxy/wait-for-it.sh auth:80 -t 0 && \
+  /code/proxy/wait-for-it.sh well-known:80 -t 0 && \
+  /code/proxy/wait-for-it.sh http-handler:80 -t 0 && \
+  nginx -g 'daemon off;'
