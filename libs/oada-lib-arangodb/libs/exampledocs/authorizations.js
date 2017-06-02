6a328b456026fe57b87a8ac8318358cc277c3879
Yaguang Zhang <yaguangz@outlook.com>
2017-06-02 10:24:48 -0400

    Add a new token to Sam with a valid scope.
      - This is for the owner test of GET.

--- libs/oada-lib-arangodb/libs/exampledocs/authorizations.js
+++ libs/oada-lib-arangodb/libs/exampledocs/authorizations.js
@@ -17,4 +17,13 @@ module.exports = [
     "user": { "_id": "users/default:users_sam_321" },
     "clientId": "jf93caauf3uzud7f308faesf3@provider.oada-dev.com"
   },
+  {
+    "_id": "authorizations/default:authorization-654",
+    "token": "def",
+    "scope": ["oada.rocks:all"],
+    "createTime": 1413831649937,
+    "expiresIn": 60,
+    "user": { "_id": "users/default:users_sam_321" },
+    "clientId": "jf93caauf3uzud7f308faesf3@provider.oada-dev.com"
+  },
 ];
