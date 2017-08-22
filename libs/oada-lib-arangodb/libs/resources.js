OADA/server

commit c613b86e4000d3aa5efa90a8d05379a631959e12

graph lookup bug fix

libs/oada-lib-arangodb/libs/resources.js
@@ -88,7 +88,9 @@ function lookupFromUrl(url) {
       if (res.vertices.length - 1 < pieces.length) {
         trace('lookupFromUrl(' + url + '):',
             'more URL pieces than vertices, computing path');
-        let extras = pieces.length - (res.vertices.length - 1);
+        let lastResource = (res.vertices.length-1) - (_.findIndex(_.reverse(res.vertices), 'is_resource'))
+          // Slice a negative value to take the last n pieces of the array 
+        path_leftover = pointer.compile(pieces.slice((lastResource) - pieces.length))
         pathLeftover = pointer.compile(pieces.slice(0 - extras));
       } else {
         trace('lookupFromUrl(' + url + '):',

commit 54bcc9ffed65d77eff610471560857824b62dedc

Fix bug introduced in c613b86e4000d3aa5efa90a8d05379a631959e12

libs/oada-lib-arangodb/libs/resources.js
@@ -88,10 +88,11 @@ function lookupFromUrl(url) {
       if (res.vertices.length - 1 < pieces.length) {
         trace('lookupFromUrl(' + url + '):',
             'more URL pieces than vertices, computing path');
-        let lastResource = (res.vertices.length-1) - (_.findIndex(_.reverse(res.vertices), 'is_resource'))
-          // Slice a negative value to take the last n pieces of the array 
-        path_leftover = pointer.compile(pieces.slice((lastResource) - pieces.length))
-        pathLeftover = pointer.compile(pieces.slice(0 - extras));
+        let lastResource = (res.vertices.length - 1) -
+            (_.findIndex(_.reverse(res.vertices), 'is_resource'));
+        // Slice a negative value to take the last n pieces of the array
+        pathLeftover =
+            pointer.compile(pieces.slice(lastResource - pieces.length));
       } else {
         trace('lookupFromUrl(' + url + '):',
             'same number URL pieces as vertices, path is on graphNode');