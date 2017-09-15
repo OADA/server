c5dd7aee78720839ec8798b256aca749c2edf4a8
Alex Layton <alex@layton.in>
2017-09-15 13:46:54 -0400

    Update permissions check to use graph traversal

--- permissions-handler/oada-srvc-permissions-handler/index.js
+++ permissions-handler/oada-srvc-permissions-handler/index.js
@@ -18,10 +18,7 @@
 const debug = require('debug');
 const warn = debug('permissions-handler:trace');
 const trace = debug('permissions-handler:trace');
-const info = debug('permissions-handler:info');
-const error = debug('permissions-handler:error');
 
-const Promise = require('bluebird');
 const Responder = require('../../libs/oada-lib-kafka').Responder;
 const oadaLib = require('../../libs/oada-lib-arangodb');
 const config = require('./config');
@@ -29,113 +26,102 @@ const config = require('./config');
 //---------------------------------------------------------
 // Kafka intializations:
 const responder = new Responder(
-			config.get('kafka:topics:permissionsRequest'),
-			config.get('kafka:topics:httpResponse'),
-			'permissions-handler');
+            config.get('kafka:topics:permissionsRequest'),
+            config.get('kafka:topics:httpResponse'),
+            'permissions-handler');
 
 module.exports = function stopResp() {
-  return responder.disconnect(); 
+  return responder.disconnect();
 };
 
-const scopeTypes = {                                                         
-  'oada.rocks': [                                                          
-		'application/vnd.oada.bookmarks.1+json',                             
-		'application/vnd.oada.shares.1+json',                                
-		'application/vnd.oada.rocks.1+json',                                 
-		'application/vnd.oada.rock.1+json',                                  
-		'application/vnd.fpad.audit.globalgap.1+json',
-	],
-	'fpad': [
-		'application/vnd.oada.bookmarks.1+json',                             
-		'application/vnd.oada.shares.1+json',                                
-		'application/vnd.oada.rocks.1+json',                                 
-		'application/vnd.oada.rock.1+json',                                  
-		'application/vnd.fpad.audit.primusgfs.1+json',
-		'application/vnd.fpad.audit.globalgap.1+json',
-		'application/vnd.fpad.certification.primusgfs.1+json',
-		'application/vnd.fpad.certification.globalgap.1+json',
-		'application/vnd.fpad.certifications.globalgap.1+json',
-		'application/vnd.fpad.client.1+json',
-		'application/vnd.fpad.clients.1+json',
-		'application/vnd.fpad.1+json',
-	]
-};                                                                           
-function scopePerm(perm, has) {                                              
-  return perm === has || perm === 'all';                                   
-}                                                                            
-                                                                                 
+const scopeTypes = {
+  'oada.rocks': [
+        'application/vnd.oada.bookmarks.1+json',
+        'application/vnd.oada.shares.1+json',
+        'application/vnd.oada.rocks.1+json',
+        'application/vnd.oada.rock.1+json',
+        'application/vnd.fpad.audit.globalgap.1+json',
+    ],
+    'fpad': [
+        'application/vnd.oada.bookmarks.1+json',
+        'application/vnd.oada.shares.1+json',
+        'application/vnd.oada.rocks.1+json',
+        'application/vnd.oada.rock.1+json',
+        'application/vnd.fpad.audit.primusgfs.1+json',
+        'application/vnd.fpad.audit.globalgap.1+json',
+        'application/vnd.fpad.certification.primusgfs.1+json',
+        'application/vnd.fpad.certification.globalgap.1+json',
+        'application/vnd.fpad.certifications.globalgap.1+json',
+        'application/vnd.fpad.client.1+json',
+        'application/vnd.fpad.clients.1+json',
+        'application/vnd.fpad.1+json',
+    ]
+};
+function scopePerm(perm, has) {
+  return perm === has || perm === 'all';
+}
+
 responder.on('request', function handleReq(req) {
-	let response = {
-		scopes: {
-			read: false,
-			write: false,
-		},
-		permissions: {
-			read: false,
-			write: false,
-			owner: false
-		}
-	}
-	return oadaLib.resources.getResource(req.oadaGraph.resource_id, '').then((resource) => {
-		//Check scopes
-		if (process.env.IGNORE_SCOPE === 'yes') {
-	    trace('IGNORE_SCOPE environment variable is true')
-			responder.scopes = { read: true, write: true };
-		} else {
-			//TODO: fix duplicated code here.......
-			response.scopes.read = req.scope.some(function chkScope(scope) {
-	      var type;
-				var perm;
-				[type, perm] = scope.split(':');
+    let response = {
+        scopes: {
+            read: false,
+            write: false,
+        },
+        permissions: {
+            read: false,
+            write: false,
+            owner: false
+        }
+    };
+    return oadaLib.resources.getResource(req.oadaGraph.resource_id, '').then((resource) => {
+        //Check scopes
+        if (process.env.IGNORE_SCOPE === 'yes') {
+            trace('IGNORE_SCOPE environment variable is true');
+            responder.scopes = { read: true, write: true };
+        } else {
+            //TODO: fix duplicated code here.......
+            response.scopes.read = req.scope.some(function chkScope(scope) {
+                var type;
+                var perm;
+                [type, perm] = scope.split(':');
 
-				if (!scopeTypes[type]) {
-					warn('Unsupported scope type "' + type + '"');
-					return false;
-				}
+                if (!scopeTypes[type]) {
+                    warn('Unsupported scope type "' + type + '"');
+                    return false;
+                }
 
-        let contentType = resource ? resource._type : req.contentType
-				return scopeTypes[type].indexOf(contentType) >= 0 && 
-					scopePerm(perm, 'read');
-			});
+                let contentType = resource ? resource._type : req.contentType;
+                return scopeTypes[type].indexOf(contentType) >= 0 &&
+                        scopePerm(perm, 'read');
+            });
 
-			response.scopes.write = req.scope.some(function chkScope(scope) {
-	      var type;
-				var perm;
-				[type, perm] = scope.split(':');
+            response.scopes.write = req.scope.some(function chkScope(scope) {
+                var type;
+                var perm;
+                [type, perm] = scope.split(':');
 
-				if (!scopeTypes[type]) {
-					warn('Unsupported scope type "' + type + '"');
-					return false;
-				}
-				let contentType = resource ? resource._type : req.contentType
-				return scopeTypes[type].indexOf(contentType) >= 0 &&
-					scopePerm(perm, 'write');
-			});
-		}
-		//Check permissions. 1. Check if owner.
-		if (resource && resource._meta._owner === req.user_id) {
-	    trace('Resource requested by owner.')
-			response.permissions = {
-				read: true,
-				write: true,
-				owner: true
-			}
-		//Check permissions. 2. Check if otherwise permissioned.
-		} else if (resource && resource._meta._permissions) { 
-			if (Object.keys(resource._meta._permissions)
-			.some(user => user === req.user_id)) {
-	      trace('User has permissions on requested resource.')
-				response.permissions = resource._meta._permissions[req.user_id]
-			}
-		} else if (req.contentType) {
-			trace('1HERE')
-			response.permissions = {
-				read: true,
-				write: true,
-				owner: true
-			}
-		}
-		trace('END RESULT', response)
-		return response
-	})
-})
+                if (!scopeTypes[type]) {
+                    warn('Unsupported scope type "' + type + '"');
+                    return false;
+                }
+                let contentType = resource ? resource._type : req.contentType;
+                return scopeTypes[type].indexOf(contentType) >= 0 &&
+                        scopePerm(perm, 'write');
+            });
+        }
+        //Check permissions. 1. Check if owner.
+        if (resource && resource._meta._owner === req.user_id) {
+            trace('Resource requested by owner.');
+            response.permissions = {
+                read: true,
+                write: true,
+                owner: true
+            };
+        //Check permissions. 2. Check if otherwise permissioned.
+        } else {
+            response.permissions = req.oadaGraph.permissions;
+        }
+        trace('END RESULT', response);
+        return response;
+    });
+});
