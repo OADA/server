99279c2c7b6ba271f516dc957ad9e8801f3770df
sanoel <noel.samuel.a@gmail.com>
2019-03-12 11:00:11 -0400

    handle do-not-run-yarn for containers/services with custom node version requirements

--- yarn/do-yarn.sh
+++ yarn/do-yarn.sh
@@ -3,6 +3,9 @@
 CDIR=`pwd`
 
 echo "----------"
+# if [ ! -f $2/../do-not-run-yarn.* ]; then
+# exit
+# fi
 echo "cd $2 && yarn $1"
 echo "----------"
 cd $2 && yarn --cache-folder /code/yarn/yarn-cache $1
