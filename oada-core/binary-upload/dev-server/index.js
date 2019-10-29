2e43e1ac9db49bcb5e7c0d5a0f8826b7b885a032
nate <nate.givens03@gmail.com>
2019-10-29 09:16:04 -0400

    feat: simple dev server to save files to uploads/
    .gitignore now ignores uploads/ folder

---
+++ oada-core/binary-upload/dev-server/index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
+// const formidable = require('formidable');
+
+const app = express();
+const port = 8000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(fileUpload());
+app.use(cors());
+
+app.get('/', (req, res) => res.json({
+  status: 'Ack',
+  res: 'GET',
+}));
+
+app.post('/', (req, res) => {
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).json({
+      status: 'Nack',
+      res: 'POST'
+    });
+  }
+
+  let sampleFile = req.files.file;
+  console.log(sampleFile);
+  sampleFile.mv(`./uploads/${sampleFile.name}`, (e) => {
+    if (e) {
+      console.error(e);
+      return res.status(500).send(e)
+    }
+    res.json({
+      status: 'Ack',
+      res: 'POST',
+    });
+  });
+  /*
+  res.json({
+    status: 'Ack',
+    res: 'POST',
+  });
+  */
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}`));
