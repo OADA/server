54e7612f27c1fb901d9318f8758a6be65a7d2470
nate <nate.givens03@gmail.com>
2019-10-31 10:31:57 -0400

    feat: dev-server checks that file uploads match originals
    This isn't particularly useful at the present and the tools should
    handle file uploads properly, but when we start breaking uploads up into
    pieces, it becomes more likely that errors will appear in recieved files

--- oada-core/binary-upload/dev-server/index.js
+++ oada-core/binary-upload/dev-server/index.js
@@ -4,6 +4,9 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
 const fs = require('fs');
+const bluebird = require('bluebird');
+
+const readfile = bluebird.promisify(fs.readFile);
 
 const app = express();
 const port = 8000;
@@ -18,26 +21,38 @@ app.get('/', (req, res) => res.json({
   res: 'GET',
 }));
 
-app.post('/', (req, res) => {
+app.post('/', async (req, res) => {
   if (!req.files || Object.keys(req.files).length === 0) {
     return res.status(400).json({
-      status: 'Nack',
+      status: 'NACK',
       res: 'POST'
     });
   }
 
-  let sampleFile = req.files.file;
-  console.log(sampleFile);
-  sampleFile.mv(`./uploads/${sampleFile.name}`, (e) => {
+  let receivedFile = req.files.file;
+  receivedFile.mv(`./uploads/${receivedFile.name}`, (e) => {
     if (e) {
       console.error(e);
       return res.status(500).send(e)
     }
+  });
+
+  let localFile = await readfile(`./to_upload/${receivedFile.name}`);
+  let recvFile = await readfile(`./uploads/${receivedFile.name}`);
+
+  // check if buffer contents are equivalent
+  if (recvFile.equals(localFile)) {
+    // ACK => file uploaded successfully
     res.json({
-      status: 'Ack',
+      status: 'ACK',
       res: 'POST',
     });
-  });
+  } else {
+    res.json({
+      status: 'NACK',
+      res: 'POST',
+    });
+  }
 });
 
 fs.mkdir(__dirname + '/uploads', { mode: 0744 }, err => {
