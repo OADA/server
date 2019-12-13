eb7e11b29264e0082d3c3d191719b34b460e53e4
Aaron Ault <ault@purdue.edu>
2019-12-12 21:09:12 -0500

    updated permissions handler to move scopes to top level and add additional-scopes that modules can insert in /code/scopes/additional-scopes

--- oada-core/permissions-handler/oada-srvc-permissions-handler/server.js
+++ oada-core/permissions-handler/oada-srvc-permissions-handler/server.js
@@ -16,13 +16,16 @@
 'use strict';
 
 const debug = require('debug');
+const fs = require('fs');
 const typeis = require('type-is');
-const warn = debug('permissions-handler:trace');
+const warn = debug('permissions-handler:warn');
+const info = debug('permissions-handler:info');
 const trace = debug('permissions-handler:trace');
 
 const Responder = require('../../libs/oada-lib-kafka').Responder;
 const oadaLib = require('../../libs/oada-lib-arangodb');
 const config = require('./config');
+const _ = require('lodash');
 
 //---------------------------------------------------------
 // Kafka intializations:
@@ -35,98 +38,26 @@ module.exports = function stopResp() {
   return responder.disconnect();
 };
 
-const scopeTypes = {
-  'oada.mirrors': [
-    'application/vnd.oada.mirrors.1:json',
-  ],
-  'oada.fields': [
-		'application/vnd.oada.bookmarks.1+json',
-		'application/vnd.oada.shares.1+json',
-		'application/vnd.oada.fields.1+json',
-		'application/vnd.oada.field.1+json',
-		'application/json',
-  ],
-  'oada.isoblue': [
-		'application/vnd.oada.bookmarks.1+json',
-		'application/vnd.oada.isoblue.1+json',
-		'application/vnd.oada.isoblue.device.1+json',
-		'application/vnd.oada.isoblue.dataset.1+json',
-		'application/vnd.oada.isoblue.day.1+json',
-		'application/vnd.oada.isoblue.hour.1+json',
-		'application/json',
-  ],
-  'oada.operations': [
-		'application/vnd.oada.bookmarks.1+json',
-		'application/vnd.oada.shares.1+json',
-		'application/vnd.oada.operation.1+json',
-		'application/vnd.oada.operations.1+json',
-		'application/json',
-  ],
-  'oada.rocks': [
-		'application/vnd.oada.bookmarks.1+json',
-		'application/vnd.oada.shares.1+json',
-		'application/vnd.oada.rocks.1+json',
-		'application/vnd.oada.rock.1+json',
-		'application/json',
-  ],
-  'trellisfw': [
-		'application/vnd.oada.bookmarks.1+json',
-		'application/vnd.oada.shares.1+json',
-		'application/vnd.oada.rocks.1+json',
-		'application/vnd.oada.rock.1+json',
-		'application/vnd.trellisfw.audit.primusgfs.1+json',
-		'application/vnd.trellisfw.audit.globalgap.1+json',
-		'application/vnd.trellisfw.certification.primusgfs.1+json',
-		'application/vnd.trellisfw.certification.globalgap.1+json',
-		'application/vnd.trellisfw.certifications.globalgap.1+json',
-		'application/vnd.trellisfw.certifications.1+json',
-		'application/vnd.trellisfw.client.1+json',
-		'application/vnd.trellisfw.clients.1+json',
-		'application/vnd.trellisfw.connection.1+json',
-		'application/vnd.trellisfw.1+json',
-		'application/json',
-	],
-  'trellis': [
-		'application/vnd.oada.bookmarks.1+json',
-		'application/vnd.oada.shares.1+json',
-		'application/vnd.oada.rocks.1+json',
-		'application/vnd.oada.rock.1+json',
-		'application/vnd.trellis.audit.primusgfs.1+json',
-		'application/vnd.trellis.audit.globalgap.1+json',
-		'application/vnd.trellis.certification.primusgfs.1+json',
-		'application/vnd.trellis.certification.globalgap.1+json',
-		'application/vnd.trellis.certifications.globalgap.1+json',
-		'application/vnd.trellis.certifications.1+json',
-		'application/vnd.trellis.client.1+json',
-		'application/vnd.trellis.clients.1+json',
-		'application/vnd.trellis.connection.1+json',
-		'application/vnd.trellis.1+json',
-		'application/json',
-	],
-  'oada.yield': [
-    'application/vnd.oada.services.1+json',
-    'application/vnd.oada.service.1+json',
-		'application/vnd.oada.bookmarks.1+json',
-		'application/vnd.oada.shares.1+json',
-		'application/vnd.oada.tiled-maps.1+json',
-		'application/vnd.oada.tiled-maps.dry-yield-map.1+json',
-		'application/vnd.oada.harvest.1+json',
-		'application/vnd.oada.as-harvested.1+json',
-		'application/vnd.oada.as-harvested.yield-moisture-dataset.1+json',
-		'application/vnd.oada.data-index.1+json',
-		'application/vnd.oada.data-index.tiled-maps.1+json',
-		'application/vnd.oada.connection.1+json',
-		'application/vnd.oada.note.1+json',
-		'application/vnd.oada.notes.1+json',
-		'application/vnd.oada.field.1+json',
-		'application/vnd.oada.fields.1+json',
-		'application/vnd.oada.grower.1+json',
-		'application/vnd.oada.farm.1+json',
-		'application/vnd.oada.yield.1+json',
-		'application/vnd.oada.as-harvested.geohash.1+json',
-		'application/json',
-	],
-};
+const scopeTypes = require('/code/scopes/builtin');
+trace('Parsed builtin scopes, they are: ', scopeTypes);
+// Augment scopeTypes by merging in anything in /scopes/additional-scopes
+const additionalScopesFiles = _.filter(
+  fs.readdirSync('/code/scopes/additional-scopes'), 
+  f => !f.match(/^\./) // remove hidden files
+);
+_.each(additionalScopesFiles, af => {
+  try { 
+    trace('Trying to add additional scope '+af);
+    const newscope = require('/code/scopes/additional-scopes/'+af);
+    _.each(_.keys(newscope), k => {
+      trace('Setting scopeTypes['+k+'] to new scope ',newscope[k]);
+      scopeTypes[k] = newscope[k]; // overwrite entire scope, or create new if doesn't exist
+    });
+  } catch(e) {
+    warn('FAILED to require(/code/scopes/additional-scopes/'+af+': error was ',e);
+  }
+});
+
 function scopePerm(perm, has) {
   return perm === has || perm === 'all';
 }
