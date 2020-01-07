b302116560be8bf05c05127e433a868c360e0c81
Nathan Givens <nate.givens03@gmail.com>
2020-01-07 08:32:03 -0500

    feat: use cacache to store/retrieve binary resources

--- oada-core/http-handler/oada-srvc-http-handler/resources.js
+++ oada-core/http-handler/oada-srvc-http-handler/resources.js
@@ -18,10 +18,17 @@ const changes = require('../../libs/oada-lib-arangodb').changes;
 const putBodies = require('../../libs/oada-lib-arangodb').putBodies;
 const OADAError = require('oada-error').OADAError;
 
-const Readable = require('stream').Readable;
-const fs = require('fs');
+const { pipeline } = require('stream');
+const pipelineAsync = require('bluebird').promisify(pipeline);
+const cacache = require('cacache');
+
 const multer = require('multer');
 const fileUpload = multer({ limits: '20mb' });
+// const Readable = require('stream').Readable;
+// const fs = require('fs');
+
+// check that path exists?
+const CACHE_PATH = "tmp/oada-cache";
 
 const config = require('./config');
 
@@ -171,7 +178,7 @@ router.get('/*', async function getChanges(req, res, next) {
     }
 });
 
-router.get('/*', function getResource(req, res, next) {
+router.get('/*', async function getResource(req, res, next) {
     // TODO: Should it not get the whole meta document?
     // TODO: Make getResource accept an array of paths and return an array of
     //       results. I think we can do that in one arango query
@@ -181,44 +188,61 @@ router.get('/*', function getResource(req, res, next) {
             req.oadaGraph['path_leftover']
     );
 
-    return Promise.join(doc, function returnDoc(doc) {
-        info("DOC IS", doc);
-        // TODO: Allow null values in OADA?
-        if (doc === undefined || doc === null) {
-            console.log("Resource not found");
-            throw new OADAError("Not Found", 404);
-        } else {
-            console.log(
-                `Resource: ${req.oadaGraph.resource_id}, Rev: ${req.oadaGraph.rev}`,
-            );
-        }
+    // TODO check json content-type or if _id end in '/_meta'
+    if ((req.oadaGraph['type'] && req.oadaGraph['type'].match(/[\/|+]json$/))
+        || req.oadaGraph['path_leftover'].match(/\/_meta$/)) {
+        return Promise.join(doc, function returnDoc(doc) {
+            info("DOC IS", doc);
+            // TODO: Allow null values in OADA?
+            if (doc === undefined || doc === null) {
+                console.log("Resource not found");
+                throw new OADAError("Not Found", 404);
+            } else {
+                console.log(
+                    `Resource: ${req.oadaGraph.resource_id}, Rev: ${req.oadaGraph.rev}`,
+                );
+            }
 
-        // XXX for now assumes only the file itself is retrieved; not a parent document
-        // TODO better check for json content-type
-        doc = unflattenMeta(doc);
-        if (doc.mimetype) {
-            res.writeHead(200, {
-                'Content-Type': doc.mimetype,
-                'Content-Length': doc.size,
-                'X-OADA-Rev': req.oadaGraph.rev
-            });
-
-            // Not sure the read function is quite what we want...
-            // May need to be more sophisticated when enabling writing partial
-            // file for changes. Right now it makes it so no more items can be
-            // pushed into the Readable stream (the stream can't ever be
-            // paused).
-            let stream = new Readable({
-                read() {
-                    this._paused = false;
-                }
-            });
-            stream.push(Buffer.from(doc.buffer.data));
-            return stream.pipe(res);
-        } else {
+            doc = unflattenMeta(doc);
+            info("doc unflattened now");
             return res.set("X-OADA-Rev", req.oadaGraph.rev).json(doc);
+            /*
+            if (doc.mimetype) {
+                res.writeHead(200, {
+                    'Content-Type': doc.mimetype,
+                    'Content-Length': doc.size,
+                    'X-OADA-Rev': req.oadaGraph.rev
+                });
+
+                // Not sure the read function is quite what we want...
+                // May need to be more sophisticated when enabling writing partial
+                // file for changes. Right now it makes it so no more items can be
+                // pushed into the Readable stream (the stream can't ever be
+                // paused).
+                let stream = new Readable({
+                    read() {
+                        this._paused = false;
+                    }
+                });
+                stream.push(Buffer.from(doc.buffer.data));
+                return stream.pipe(res);
+            } else {
+                return res.set("X-OADA-Rev", req.oadaGraph.rev).json(doc);
+            }
+            */
+        }).catch(next);
+    } else {
+        // get binary
+        if (req.oadaGraph['path_leftover']) {
+            info(req.oadaGraph['path_leftover']);
+            throw new OADAError("Not final document");
         }
-    }).catch(next);
+
+        await pipelineAsync(
+            cacache.get.stream(CACHE_PATH, req.oadaGraph["resource_id"]),
+            res,
+        );
+    }
 });
 
 // TODO: This was a quick make it work. Do what you want with it.
@@ -268,26 +292,16 @@ function noModifyShares(req, res, next) {
 router.delete('/*', noModifyShares);
 router.put('/*', noModifyShares);
 
+/*
 router.put('/*', fileUpload.single('file'), async function convertFile(req, res, next) {
-    const content_type = req.headers['content-type'];
-    if (content_type && content_type !== 'application/json') {
+    if (req.header('content-type') && req.header('content-type').match(/[\/|+]json$/)) {
+    } else {
         info('Non-JSON resource detected');
-        // req.file.buffer = Array.from(req.file.buffer);
-        // req.body.file = {
-        //     array_buf: Array.from(req.file.buffer),
-        //     ...req.file,
-        // };
-        // delete req.body.buffer
-        // TODO is this where the file object should be placed to store properly
-        // req.body.file = req.file;
-        // TODO not sure this is the best way to let body parser handle request
-        // res.set('Content-Type', 'application/json');
-        // const arr = Array.from(req.file.buffer);
-        // req.body.arr = arr;
-        req.body = JSON.stringify(req.file);
+        req.body = {};
     }
     next();
 });
+*/
 
 // TODO check if not json/+json -> handle as binary resource
 // Parse JSON content types as text (but do not parse JSON yet)
@@ -297,6 +311,7 @@ router.put('/*', bodyParser.text({
     limit: '20mb',
 }));
 
+/*
 router.put('/*', function checkBodyParsed(req, res, next) {
     let err = null;
 
@@ -311,6 +326,7 @@ router.put('/*', function checkBodyParsed(req, res, next) {
 
     return next(err);
 });
+*/
 
 router.put('/*', async function ensureTypeTreeExists(req, res, next) {
     if (req.headers['x-oada-bookmarks-type']) {
@@ -412,8 +428,18 @@ router.put('/*', async function ensureTypeTreeExists(req, res, next) {
     }
 })
 
-router.put('/*', function putResource(req, res, next) {
+router.put('/*', async function putResource(req, res, next) {
     info(`Saving PUT body for request ${req.id}`);
+    if (req.header('content-type') && !req.header('content-type').match(/[\/|+]json$/)) {
+        // put binary
+        await pipelineAsync(
+            req,
+            cacache.put.stream(CACHE_PATH, req.oadaGraph.resource_id),
+        );
+
+        req.body = '{}';
+    }
+
     return putBodies.savePutBody(req.body)
         .tap(() => info(`PUT body saved for request ${req.id}`))
         .get('_id')
@@ -448,9 +474,9 @@ router.put('/*', function putResource(req, res, next) {
                 case 'permission':
                     return Promise.reject(new OADAError('Forbidden', 403,
                             'User does not own this resource'));
-		case 'if-match failed':
-			return Promise.reject(new OADAError('Precondition Failed', 412,
-				'If-Match header does not match current resource _rev'));
+        case 'if-match failed':
+            return Promise.reject(new OADAError('Precondition Failed', 412,
+                'If-Match header does not match current resource _rev'));
                 default:
                     let msg = 'write failed with code ' + resp.code;
                     return Promise.reject(new OADAError(msg));
@@ -462,6 +488,18 @@ router.put('/*', function putResource(req, res, next) {
                 .redirect(204, req.baseUrl + req.url);
         })
         .catch(next);
+
+    // } else {
+    //     // put binary
+    //     await pipelineAsync(
+    //         req,
+    //         cacache.put.stream(CACHE_PATH, req.oadaGraph.resource_id),
+    //     );
+
+    //     // TODO: what does savePutBody expect in the object?
+    //     // TODO: potentially write resource_id to
+    //     putBodies.savePutBody({});
+    // }
 });
 
 // Don't let users DELETE their bookmarks?
