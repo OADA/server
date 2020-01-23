f17d7631734ac2bd8c64de67a7f36896f79172c5
Andrew Balmos <andrew@balmos.org>
2020-01-23 16:36:47 -0500

    Add missing comma

--- scopes/builtin.js
+++ scopes/builtin.js
@@ -40,7 +40,7 @@ module.exports = {
     'application/vnd.oada.shares.1+json',
     'application/vnd.oada.rocks.1+json',
     'application/vnd.oada.rock.1+json',
-    'application/vnd.oada.service.jobs.1+json'
+    'application/vnd.oada.service.jobs.1+json',
     'application/vnd.trellisfw.audit.primusgfs.1+json',
     'application/vnd.trellisfw.audit.globalgap.1+json',
     'application/vnd.trellisfw.certification.primusgfs.1+json',
