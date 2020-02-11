ce26402402d0ff20567f065e0f716ee1702b1fef
Aaron Ault <aultac@gmail.com>
2020-02-11 17:21:34 -0500

    Updated initiate-letsencrypt-alldomains to ignore domains-enabled which have a certs/ folder inside them

--- oada-core/admin/initiate-letsencrypt-alldomains.sh
+++ oada-core/admin/initiate-letsencrypt-alldomains.sh
@@ -13,8 +13,29 @@ echo "Asking letsencrypt for new domains..."
 
 # To get new certificates entirely (not just renew, don't forget to remove --staging when done testing):
 # If you are adding domains, you need to also do --expand
-certbot certonly --expand --webroot --webroot-path /var/www/letsencrypt --agree-tos -m aaron@openag.io --noninteractive --domains $(ls -1 /domains-enabled | sed 's/localhost//' | sed '/^$/d' | tr '\n' ',' | sed 's/,$//')
+COMMAND="ls -1 /domains-enabled | sed 's/localhost//' "
+for i in $(ls -1 /domains-enabled); do
+  if [ -e "/domains-enabled/$i/certs" ]; then
+    echo "Domain $i has a certs folder for a manual cert, removing it from the list of Lets Encrypt domains"
+    COMMAND="${COMMAND} | sed s'/$i//' "
+  else
+    echo "Domain $i does not have a certs folder, including it in the list to request certs from LetsEncrypt"
+  fi
+done
+# Now eliminate empty lines
+COMMAND="${COMMAND}| sed '/^$/d' | tr '\n' ',' | sed 's/,$//'"
+# Eval the command to get the list of valid domains w/o certs, one per line:
+DOMAINS=$(eval $COMMAND)
 
+# For certbot list, join with commas, strip trailing comma:
+COMMA_SEPARATED_DOMAINS_COMMAND="${COMMAND} | tr '\n' ',' | sed 's/,$//'"
+COMMA_SEPARATED_DOMAINS=$(eval $COMMA_SEPARATED_DOMAINS_COMMAND)
+echo "Final domains list for lets encrypt cert is: $COMMA_SEPARATED_DOMAINS"
+echo "-----------"
+
+# Now run the actual certbot command:
+echo "certbot certonly --expand --webroot --webroot-path /var/www/letsencrypt --agree-tos -m aaron@openag.io --noninteractive --domains $COMMA_SEPARATED_DOMAINS"
+certbot certonly --expand --webroot --webroot-path /var/www/letsencrypt --agree-tos -m aaron@openag.io --noninteractive --domains $COMMA_SEPARATED_DOMAINS
 
 # THEN you have to symlink /etc/letsencrypt/live/<something>/*.pem to proxy's /certs/<domain>/.
 if [ ! -e '/certs/live' ]; then
@@ -26,7 +47,7 @@ fi
 LETSENCRYPT_CERTNAME=`ls -t /certs/live | sed '/^\s*README\s*$/d' | head -1`
 cd /certs
 # the "-t" in ls sorts with most recent on top, and we take that one
-for d in $(ls -1 /domains-enabled | sed 's/localhost//'); do
+for d in $DOMAINS; do
   echo "Symlinking /certs/live/$LETSENCRYPT_CERTNAME to /certs/$d"
   ln -s ./live/$LETSENCRYPT_CERTNAME ./$d
 done
