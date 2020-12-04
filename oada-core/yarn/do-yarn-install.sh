72ab6869402eb67e075009f54be83074293ed952
Alex Layton <alex@layton.in>
2020-12-04 11:28:48 -0500

    Remove yarn container
    Not needed now. Containers actually have their code.

--- oada-core/yarn/do-yarn-install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cd /code/yarn && \
-mkdir -p yarn-cache && \
-find /code -maxdepth 3 -path *node_modules* -prune -o -name package.json -exec dirname {} \; | \
-  xargs -n 1 /code/yarn/do-yarn.sh install
--- oada-core/yarn/do-yarn-upgrade.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-find /code -name package.json -exec dirname {} \; |
-  grep -v node_modules | xargs -n 1 do-yarn.sh upgrade
