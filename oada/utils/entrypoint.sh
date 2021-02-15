e00aaa4ab464753dca10acbf40ee1ccba2339337
Alex Layton <alex@layton.in>
2021-02-15 18:02:32 -0500

    Switch service logging to pino using pino-debug
    Defaults to pino-pretty transport,
    but it can be changed via environment var `PINO_TRANSPORT`.

--- oada/utils/entrypoint.sh
+++ oada/utils/entrypoint.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 
+START="yarn workspace @oada/$OADA_SERVICE start --config=/oada-srvc-docker-config.js"
 (expr $OADA_SERVICE = startup || /wait-for-it.sh startup:80 -t 0) && \
-  yarn workspace @oada/$OADA_SERVICE start --config=/oada-srvc-docker-config.js
+    ([ -z "$PINO_TRANSPORT" ] && $START) || $START | $PINO_TRANSPORT
