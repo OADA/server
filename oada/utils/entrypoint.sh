dc598f4ff46c3156bdfa8ec2f15d8ed21ab27a1c
Alex Layton <alex@layton.in>
2021-03-09 00:34:24 -0500

    Fix for entrypoint
    It incorrectly handled the case of empty PINO_TRANSPORT
    when CMD exited with an error.

--- oada/utils/entrypoint.sh
+++ oada/utils/entrypoint.sh
@@ -1,5 +1,10 @@
 #! /bin/sh
 
 CMD="yarn workspace @oada/${OADA_SERVICE} run $@"
-(expr ${OADA_SERVICE} = startup || /wait-for-it.sh startup:80 -t 0) && \
-    ([ -z "${PINO_TRANSPORT}" ] && ${CMD}) || ${CMD} | ${PINO_TRANSPORT}
+(expr ${OADA_SERVICE} = startup || /wait-for-it.sh startup:80 -t 0) &&
+    if [ -z "${PINO_TRANSPORT}" ];
+    then
+        ${CMD};
+    else
+        ${CMD} | ${PINO_TRANSPORT};
+    fi
