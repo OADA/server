4192f2ff0e17b5602210ef75cdfab7b3a830601e
Alex Layton <alex@layton.in>
2021-04-23 16:29:36 -0400

    Try KafkaJS instead of node-rdkafka

--- oada/libs/oada-lib-kafka/base.js
+++ oada/libs/oada-lib-kafka/base.js
@@ -13,14 +13,11 @@
  * limitations under the License.
  */
 
-'use strict';
-
 const process = require('process');
 const EventEmitter = require('events');
 
 const Bluebird = require('bluebird');
-const ksuid = require('ksuid');
-const kf = require('node-rdkafka');
+const { Kafka } = require('kafkajs');
 
 const config = require('./config');
 const info = require('debug')('@oada/lib-kafka:info');
@@ -28,16 +25,9 @@ const error = require('debug')('@oada/lib-kafka:error');
 
 const REQ_ID_KEY = 'connection_id';
 const CANCEL_KEY = 'cancel_request';
-// Not sure I like the way options are oragnised, but it works
-const rdkafkaOpts = Object.assign(config.get('kafka:librdkafka'), {
-    'metadata.broker.list': config.get('kafka:broker'),
-});
 
 const CONNECT = Symbol('kafka-lib-connect');
 const DATA = Symbol('kafa-lib-data');
-
-const pollInterval = 500;
-const healthInterval = 5 * 60 * 1000;
 
 function topicTimeout(topic) {
     let timeout = config.get('kafka:timeouts:default');
@@ -69,77 +59,55 @@ class Base extends EventEmitter {
 
         this.requests = {};
 
+        this.kafka = new Kafka({
+            brokers: config.get('kafka:broker').split(','),
+        });
+
         this.consumer =
             consumer ||
-            new kf.KafkaConsumer({
-                'group.id': this.group,
-                ...rdkafkaOpts,
+            this.kafka.consumer({
+                groupId: this.group,
             });
-        this.producer =
-            producer ||
-            new kf.Producer({
-                dr_cb: false, //delivery report callback
-                ...rdkafkaOpts,
-            });
+        this.producer = producer || this.kafka.producer();
 
-        this.consumer.on('error', (...args) => super.emit('error', ...args));
-        this.producer.on('error', (...args) => super.emit('error', ...args));
-        this.producer.on('delivery-report', function (err, _report) {
-            if (err) error('!!!!!!!!!!!!!!!!!!!!!!! %O', err);
-        });
-
-        this.consumer.on('event.error', (...args) =>
-            super.emit('error', ...args)
-        );
-        this.producer.on('event.error', (...args) =>
-            super.emit('error', ...args)
-        );
-
-        const consumerReady = Bluebird.fromCallback((done) => {
-            this.consumer.on('ready', () => {
-                info(`${this.group}'s consumer ready`);
-                done();
-            });
-        });
-        const producerReady = Bluebird.fromCallback((done) => {
-            this.producer.on('ready', () => {
-                this.producer.setPollInterval(
-                    config.get('kafka:producer:pollInterval') || pollInterval
-                );
-                info(`${this.group}'s producer ready`);
-                // Health loop to keep the broker alive.
-                setInterval(() => {
-                    var value = Buffer.from(produceTopic + 'is alive.');
-                    //TODO: other health messages here
-                    this.producer.produce(
-                        'health',
-                        0,
-                        value,
-                        ksuid.randomSync().string
-                    );
-                }, config.get('kafka:producer:healthIterval') || healthInterval);
-                done();
-            });
-        });
-        this.ready = Bluebird.join(consumerReady, producerReady);
-
-        // Error handling stuff?
-        super.on('error', die);
         // see: https://github.com/Blizzard/node-rdkafka/issues/222
         // says fixed, but seems to still be an issue for us.
-        process.on('uncaughtExceptionMonitor', () => {
+        process.on('uncaughtExceptionMonitor', async () => {
             error('Disconnect kafka clients due to uncaught exception');
             // Disconnect kafka clients on uncaught exception
             try {
-                this.consumer.disconnect();
+                await this.consumer.disconnect();
             } catch (err) {
                 error(err);
             }
             try {
-                this.producer.disconnect();
+                await this.producer.disconnect();
             } catch (err) {
                 error(err);
             }
+        });
+
+        this.ready = Bluebird.fromCallback((done) => {
+            this[CONNECT] = async () => {
+                try {
+                    await this.consumer.connect();
+                    await this.producer.connect();
+
+                    this.consumer.subscribe({ topic: this.consumeTopic });
+                    this.consumer.run({
+                        eachMessage: async ({
+                            message: { value, ...data },
+                        }) => {
+                            // Assume all messages are JSON
+                            const resp = JSON.parse(value);
+                            super.emit(DATA, resp, data);
+                        },
+                    });
+                } catch (err) {
+                    return done(err);
+                }
+                done();
+            };
         });
     }
 
@@ -151,49 +119,23 @@ class Base extends EventEmitter {
         super.on(event, listener);
     }
 
-    async [CONNECT]() {
-        // Assume all messages are JSON
-        this.consumer.on('data', ({ value, ...data }) => {
-            const resp = JSON.parse(value);
-            super.emit(DATA, resp, data);
-        });
-
-        this.consumer.connect();
-        this.producer.connect();
-        await this.ready;
-
-        const topics = Array.isArray(this.consumeTopic)
-            ? this.consumeTopic
-            : [this.consumeTopic];
-        this.consumer.subscribe(topics);
-        this.consumer.consume();
-
-        super.emit('ready');
-    }
-
     async produce({ mesg, topic, part = null }) {
         // Assume all messages are JSON
-        const payload = JSON.stringify({
+        const value = JSON.stringify({
             time: Date.now(),
             group: this.group,
             ...mesg,
         });
-        const value = Buffer.from(payload);
 
-        await this.ready;
-
-        return this.producer.produce(topic || this.produceTopic, part, value);
+        return this.producer.send({
+            topic: topic || this.produceTopic,
+            messages: [{ value }],
+        });
     }
 
-    disconnect() {
-        const dcons = Bluebird.fromCallback((done) => {
-            this.consumer.disconnect(() => done());
-        });
-        const dprod = Bluebird.fromCallback((done) => {
-            this.producer.disconnect(() => done());
-        });
-
-        return Bluebird.join(dcons, dprod);
+    async disconnect() {
+        await this.consumer.disconnect();
+        await this.producer.disconnect();
     }
 }
 
