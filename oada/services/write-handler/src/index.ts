b3bf5e71c57d42a7a3bf4160cfd164af3ad039a8
Alex Layton <alex@layton.in>
2022-07-01 13:26:41 -0400

    style(write-handler): rework code

--- oada/services/write-handler/src/index.ts
+++ oada/services/write-handler/src/index.ts
@@ -15,6 +15,8 @@
  * limitations under the License.
  */
 
+import { config } from './config.js';
+
 import {
   ArangoError,
   ArangoErrorCode,
@@ -24,12 +26,9 @@ import {
 } from '@oada/lib-arangodb';
 import { KafkaBase, Responder } from '@oada/lib-kafka';
 
-import type Change from '@oada/types/oada/change/v2';
-import type Resource from '@oada/types/oada/resource';
+import type Change from '@oada/types/oada/change/v2.js';
+import type Resource from '@oada/types/oada/resource.js';
 
-import { config } from './config.js';
-
-import Bluebird from 'bluebird';
 import Cache from 'timed-cache';
 import { JsonPointer } from 'json-ptr';
 import debug from 'debug';
@@ -199,263 +198,246 @@ async function checkPreconditions(request: WriteRequest) {
   }
 }
 
+/**
+ * @todo Better return type for this function
+ */
+async function doWrite(
+  request: WriteRequest,
+  body: unknown
+): Promise<{ id: string; rev?: number; orev?: number; changeId?: string }> {
+  let changeType: Change[0]['type'];
+  let id = request.resource_id.replace(/^\//, '');
+
+  trace({ id, body }, 'Writing body');
+  await checkPreconditions(request);
+
+  const cacheRev =
+    cache.get(request.resource_id) ??
+    ((await resources.getResource(
+      request.resource_id,
+      '/_rev'
+    )) as unknown as number);
+
+  if (request.rev && cacheRev !== request.rev) {
+    throw new Error('rev mismatch');
+  }
+
+  let path = JsonPointer.decode(request.path_leftover);
+  let method = resources.putResource;
+  changeType = 'merge';
+  const object: DeepPartial<Resource> = {};
+
+  // Perform delete
+  if (body === undefined) {
+    trace('Body is undefined, doing delete');
+    if (path.length > 0) {
+      trace('Delete path = %s', path);
+      // TODO: This is gross
+      const aPath = Array.from(path);
+      method = async (pid, partial) =>
+        resources.deletePartialResource(pid, aPath, partial);
+      trace(
+        'Setting method = deletePartialResource(%s, %o, %O)',
+        id,
+        aPath,
+        object
+      );
+      body = null;
+      changeType = 'delete';
+      trace(`Setting changeType = 'delete'`);
+    } else {
+      if (!request.resourceExists) {
+        return { id, rev: undefined, orev: undefined, changeId: undefined };
+      }
+
+      trace('deleting resource altogether');
+      const orev = await resources.deleteResource(id);
+      return { id, orev };
+    }
+  }
+
+  const ts = Date.now() / 1000;
+  // TODO: Sanitize keys?
+
+  trace(
+    '%s: Checking if resource exists (req.resourceExists = %s)',
+    request.resource_id,
+    request.resourceExists
+  );
+  if (request.resourceExists === false) {
+    trace(
+      'initializing arango: resource_id = %s, path_leftover = %s',
+      request.resource_id,
+      request.path_leftover
+    );
+    id = request.resource_id.replace(/^\//, '');
+    path = path.slice(2);
+
+    // Initialize resource stuff
+    Object.assign(object, {
+      _type: request.contentType,
+      _meta: {
+        _id: `${id}/_meta`,
+        _type: request.contentType,
+        _owner: request.user_id,
+        stats: {
+          createdBy: request.user_id,
+          created: ts,
+        },
+      },
+    });
+    trace({ resource: object }, 'Intializing resource');
+  }
+
+  // Create object to recursively merge into the resource
+  trace({ path }, 'Recursively merging path into arango object');
+  const endK = path.pop();
+  if (endK === undefined) {
+    objectAssignDeep(object, body);
+  } else {
+    let o = object as Record<string, unknown>;
+    for (const k of path) {
+      trace('Adding path for key %s', k);
+      if (!(k in o)) {
+        // TODO: Support arrays better?
+        o[k] = {};
+      }
+
+      o = o[k] as Record<string, unknown>;
+    }
+
+    o[endK] = body as DeepPartial<Resource>;
+  }
+
+  trace({ body: object }, 'Setting body on arango object');
+
+  // Update meta
+  const meta: Partial<Resource['_meta']> & Record<string, unknown> = {
+    modifiedBy: request.user_id,
+    modified: ts,
+  };
+  object._meta = objectAssignDeep(object._meta ?? {}, meta);
+
+  // Increment rev number
+  let rev = Number.parseInt((cacheRev || 0) as string, 10) + 1;
+
+  // If rev is supposed to be set to 1, this is a "new" resource.
+  // However, change feed could still be around from an earlier delete,
+  // so check that and set rev to more than biggest one
+  if (rev === 1) {
+    const changerev = await changes.getMaxChangeRev(id);
+    if (changerev && changerev > 1) {
+      rev = Number(changerev) + 1;
+      trace(
+        'Found old changes (max rev %d) for new resource, setting initial _rev to %d include them',
+        changerev,
+        rev
+      );
+    }
+  }
+
+  object._rev = rev;
+  metaPointers.rev.set(object, rev, true);
+
+  /**
+   * ???: Error is body contains a non-matching `_id`
+   */
+  if ('_id' in object && object._id !== id) {
+    const tError = new Error(
+      `Tried to write _id ${object._id} to resource with _id ${id}`
+    );
+    throw Object.assign(tError, { code: 'bad request' });
+  }
+
+  // Compute new change
+  const children = request.from_change_id ?? [];
+  trace({ change: object }, 'Putting change');
+  const changeId = await changes.putChange({
+    change: { ...object, _rev: rev },
+    resId: id,
+    rev,
+    type: changeType,
+    children,
+    path: request.change_path,
+    userId: request.user_id,
+    authorizationId: request.authorizationid,
+  });
+  metaPointers.changes.set(
+    object,
+    {
+      _id: `${id}/_meta/_changes`,
+      _rev: rev,
+    },
+    true
+  );
+
+  // Update rev of meta?
+  object._meta._rev = rev;
+
+  const orev = await method(id, object, !request.ignoreLinks);
+  return {
+    id,
+    rev,
+    orev,
+    changeId,
+  };
+}
+
 export async function handleRequest(
   request: WriteRequest
 ): Promise<WriteResponse> {
   request.source = request.source ?? '';
   // Fixes bug if this is undefined
   request.resourceExists = request.resourceExists ?? false;
-  let id = request.resource_id.replace(/^\//, '');
-
-  // Get body and check permission in parallel
-  info('Handling %s', id);
-  const bodyP = request.bodyid
-    ? putBodies.getPutBody(request.bodyid)
-    : Promise.resolve(request.body);
-
-  trace('PUTing to "%s" in "%s"', request.path_leftover, id);
-
-  let changeType: Change[0]['type'];
-  // eslint-disable-next-line promise/valid-params
-  const upsert = Bluebird.resolve(bodyP)
-    .then(
-      async (
-        body: unknown
-      ): Promise<{ rev?: number; orev?: number; changeId?: string }> => {
-        trace({ body }, 'FIRST BODY');
-        await checkPreconditions(request);
-
-        let cacheRev = cache.get(request.resource_id);
-        if (!cacheRev) {
-          cacheRev = (await resources.getResource(
-            request.resource_id,
-            '/_rev'
-          )) as unknown as number;
-        }
-
-        if (request.rev && cacheRev !== request.rev) {
-          throw new Error('rev mismatch');
-        }
-
-        let path = JsonPointer.decode(request.path_leftover);
-        let method = resources.putResource;
-        changeType = 'merge';
-        const object: DeepPartial<Resource> = {};
-
-        // Perform delete
-        if (body === undefined) {
-          trace('Body is undefined, doing delete');
-          if (path.length > 0) {
-            trace('Delete path = %s', path);
-            // TODO: This is gross
-            const aPath = Array.from(path);
-            method = async (pid, partial) =>
-              resources.deletePartialResource(pid, aPath, partial);
-            trace(
-              'Setting method = deletePartialResource(%s, %o, %O)',
-              id,
-              aPath,
-              object
-            );
-            body = null;
-            changeType = 'delete';
-            trace(`Setting changeType = 'delete'`);
-          } else {
-            if (!request.resourceExists) {
-              return { rev: undefined, orev: undefined, changeId: undefined };
-            }
-
-            trace('deleting resource altogether');
-            const orev = await resources.deleteResource(id);
-            return { orev };
-          }
-        }
-
-        const ts = Date.now() / 1000;
-        // TODO: Sanitize keys?
-
-        trace(
-          '%s: Checking if resource exists (req.resourceExists = %s)',
-          request.resource_id,
-          request.resourceExists
-        );
-        if (request.resourceExists === false) {
-          trace(
-            'initializing arango: resource_id = %s, path_leftover = %s',
-            request.resource_id,
-            request.path_leftover
-          );
-          id = request.resource_id.replace(/^\//, '');
-          path = path.slice(2);
-
-          // Initialize resource stuff
-          Object.assign(object, {
-            _type: request.contentType,
-            _meta: {
-              _id: `${id}/_meta`,
-              _type: request.contentType,
-              _owner: request.user_id,
-              stats: {
-                createdBy: request.user_id,
-                created: ts,
-              },
-            },
-          });
-          trace({ resource: object }, 'Intializing resource');
-        }
-
-        // Create object to recursively merge into the resource
-        trace(path, 'Recursively merging path into arango object');
-        const endK = path.pop();
-        if (endK === undefined) {
-          objectAssignDeep(object, body);
-        } else {
-          let o = object as Record<string, unknown>;
-          for (const k of path) {
-            trace('Adding path for key %s', k);
-            if (!(k in o)) {
-              // TODO: Support arrays better?
-              o[k] = {};
-            }
-
-            o = o[k] as Record<string, unknown>;
-          }
-
-          o[endK] = body as DeepPartial<Resource>;
-        }
-
-        trace({ body: object }, 'Setting body on arango object');
-
-        // Update meta
-        const meta: Partial<Resource['_meta']> & Record<string, unknown> = {
-          modifiedBy: request.user_id,
-          modified: ts,
-        };
-        object._meta = objectAssignDeep(object._meta ?? {}, meta);
-
-        // Increment rev number
-        let rev = Number.parseInt((cacheRev || 0) as string, 10) + 1;
-
-        // If rev is supposed to be set to 1, this is a "new" resource.  However,
-        // change feed could still be around from an earlier delete, so check that
-        // and set rev to more than biggest one
-        if (rev === 1) {
-          const changerev = await changes.getMaxChangeRev(id);
-          if (changerev && changerev > 1) {
-            rev = Number(changerev) + 1;
-            trace(
-              'Found old changes (max rev %d) for new resource, setting initial _rev to %d include them',
-              changerev,
-              rev
-            );
-          }
-        }
-
-        object._rev = rev;
-        metaPointers.rev.set(object, rev, true);
-
-        /**
-         * ???: Error is body contains a non-matching `_id`
-         */
-        if ('_id' in object && object._id !== id) {
-          const tError = new Error(
-            `Tried to write _id ${object._id} to resource with _id ${id}`
-          );
-          throw Object.assign(tError, { code: 'bad request' });
-        }
-
-        // Compute new change
-        const children = request.from_change_id ?? [];
-        trace({ change: object }, 'Putting change');
-        const changeId = await changes.putChange({
-          change: { ...object, _rev: rev },
-          resId: id,
-          rev,
-          type: changeType,
-          children,
-          path: request.change_path,
-          userId: request.user_id,
-          authorizationId: request.authorizationid,
-        });
-        metaPointers.changes.set(
-          object,
-          {
-            _id: `${id}/_meta/_changes`,
-            _rev: rev,
-          },
-          true
-        );
-
-        // Update rev of meta?
-        object._meta._rev = rev;
-
-        const orev = await method(id, object, !request.ignoreLinks);
-        return {
-          rev,
-          orev,
-          changeId,
-        };
-      }
-    )
-    .then(
-      ({
-        rev,
-        orev,
-        changeId,
-      }: {
-        rev?: number;
-        orev?: number;
-        changeId?: string;
-      }) => {
-        if (rev) {
-          // Put the new rev into the cache
-          cache.put(id, rev);
-        }
-
-        const response: WriteResponse = {
-          msgtype: 'write-response',
-          code: 'success',
-          resource_id: id,
-          _rev: typeof rev === 'number' ? rev : 0,
-          _orev: orev,
-          user_id: request.user_id,
-          authorizationid: request.authorizationid,
-          path_leftover: request.path_leftover,
-          contentType: request.contentType,
-          indexer: request.indexer,
-          change_id: changeId,
-        };
-        // Causechain comes from rev-graph-update
-        if (request.causechain) {
-          // Pass through causechain if present
-          response.causechain = request.causechain;
-        }
-
-        return response;
-      }
-    )
-    // eslint-disable-next-line github/no-then
-    .catch(
-      {
-        name: ArangoError.name,
-        errorNum: ArangoErrorCode.ARANGO_DOCUMENT_NOT_FOUND,
-      },
-      (cError: unknown) => {
-        // Resource not found in DB
-        throw Object.assign(cError as Error, { code: 'not_found' });
-      }
-    );
+  const oid = request.resource_id.replace(/^\//, '');
 
   try {
-    // TODO: Better return type for this function
-    return await upsert;
-  } catch (cError: unknown) {
-    // Send errors over kafka
-    error({ error: cError });
-    const { code, message = 'error' } = cError as {
-      code?: string;
-      message?: string;
+    // Get body and check permission in parallel
+    info('Handling %s', oid);
+    const body = request.bodyid
+      ? await putBodies.getPutBody(request.bodyid)
+      : request.body;
+
+    trace('PUTing to "%s" in "%s"', request.path_leftover, oid);
+
+    const { id, rev, orev, changeId } = await doWrite(request, body);
+
+    if (rev) {
+      // Put the new rev into the cache
+      cache.put(id, rev);
+    }
+
+    const response: WriteResponse = {
+      msgtype: 'write-response',
+      code: 'success',
+      resource_id: id,
+      _rev: typeof rev === 'number' ? rev : 0,
+      _orev: orev,
+      user_id: request.user_id,
+      authorizationid: request.authorizationid,
+      path_leftover: request.path_leftover,
+      contentType: request.contentType,
+      indexer: request.indexer,
+      change_id: changeId,
     };
+    // Causechain comes from rev-graph-update
+    if (request.causechain) {
+      // Pass through causechain if present
+      response.causechain = request.causechain;
+    }
+
+    return response;
+  } catch (cError: unknown) {
+    const { code, message = 'error' } =
+      cError instanceof ArangoError &&
+      cError.errorNum === ArangoErrorCode.ARANGO_DOCUMENT_NOT_FOUND
+        ? { ...cError, code: 'not_found' }
+        : (cError as {
+            code?: string;
+            message?: string;
+          });
+    // Send errors over kafka
+    error({ request, error: cError }, message);
     // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
     return {
       msgtype: 'write-response',
