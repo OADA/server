770a5441ad7e940fa04c1db04704a5cbb22215f1
Alex Layton <alex@layton.in>
2023-05-02 16:04:13 -0400

    feat(import): add arango import command

---
+++ oada/libs/lib-arangodb/src/import.ts
@@ -0,0 +1,60 @@
+/**
+ * @license
+ * Copyright 2023 Open Ag Data Alliance
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { config } from './config.js';
+
+import { Database, aql } from 'arangojs';
+import debug from 'debug';
+
+import { db } from './db.js';
+
+const info = debug('@oada/lib-arangodb:import:info');
+const trace = debug('@oada/lib-arangodb:import:trace');
+
+const {
+  auth,
+  connectionString: url,
+  database: databaseName,
+} = config.get('arangodbImport');
+
+const collections = config.get('arangodb.collections');
+
+const importDb = new Database({ auth, url, databaseName });
+
+interface T {
+  _id: string;
+  _key: string;
+}
+
+for await (const { name } of Object.values(collections)) {
+  const importCollection = importDb.collection<T>(name);
+  const cursor = await importDb.query<T>(aql`
+    FOR doc IN ${importCollection}
+      RETURN doc
+  `);
+
+  const collection = db.collection<T>(name);
+  const { count } = collection;
+  let imported = 0;
+  for await (const doc of cursor) {
+    await collection.replace(doc, doc);
+    imported++;
+    trace({ imported, count, doc }, 'imported doc');
+  }
+
+  info(`${imported}/${count} documents imported from collection ${name}`);
+}
